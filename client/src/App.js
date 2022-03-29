5549917417cb1934278a339e004b479cc5d04fdf
HopeMashal <mashal.hope@gmail.com>
2022-03-29 19:29:36 +0300

    add few files and edit the app files in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,31 @@
-import { useContext } from "react";
+/* import { useContext } from "react"; */
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
+  /* Redirect, */
 } from "react-router-dom";
 
-import { AuthContext } from "./context/AuthContext";
+/* import { AuthContext } from "./context/AuthContext"; */
 
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import ForgetPassword from "./pages/ForgetPassword/ForgetPassword";
+import Navbar from "./components/Navbar/Navbar";
+import Catalog from "./pages/Catalog/Catalog";
+import Details from "./pages/Details/Details";
 
 import './App.css';
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const {user}=useContext(AuthContext)
+  /* const {user}=useContext(AuthContext) */
   return (
     <Router>
       <Navbar/>
       <Switch>
-        <Route exact path="/">
+        {/* <Route exact path="/">
           {user ? <Home/> : <Register/>}
         </Route>
         <Route path="/login">
@@ -34,6 +36,36 @@ function App() {
         </Route>
         <Route path="/forget">
           {user ? <Redirect to="/"/> :<ForgetPassword/>}
+        </Route> 
+        <Route path='/:category/search/:keyword'>
+          {user ? <Catalog/> : <Register/>}
+        </Route>
+        <Route path='/:category/:id'>
+          {user ? <Details/> : <Register/>}
+        </Route>
+        <Route path='/:category'>
+          {user ? <Catalog/> : <Register/>}
+        </Route>*/}
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/register">
+          <Register/>
+        </Route>
+        <Route path="/forget">
+          <ForgetPassword/>
+        </Route>
+        <Route path='/:category/search/:keyword'>
+          <Catalog/>
+        </Route>
+        <Route path='/:category/:id'>
+          <Details/>
+        </Route>
+        <Route path='/:category'>
+          <Catalog/>
         </Route>
       </Switch>
     </Router>
