df146f3a2ec1dd6581aceaae9a0750462ab7a8bb
HopeMashal <mashal.hope@gmail.com>
2022-04-16 14:46:10 +0300

    edit heroslide files

--- client/src/components/HeroSlide/HeroSlide.jsx
+++ client/src/components/HeroSlide/HeroSlide.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect} from 'react';
 import { useHistory } from 'react-router';
 
 import SwiperCore, { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import Button from '../Button/Button';
-import Modal, { ModalContent } from '../Modal/Modal';
 
 import tmdbApi, { movieType } from '../../apis/apiTMDB';
 import apiConfig from '../../apis/apiConfig';
@@ -14,6 +13,30 @@ import './heroSlide.css';
 
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
+
+import { useTranslation } from 'react-i18next'
+import cookies from 'js-cookie'
+
+const languages = [
+    {
+      code: 'en',
+      name: 'English',
+      country_code: 'gb',
+    },
+    {
+      code: 'ar',
+      name: 'العربية',
+      dir: 'rtl',
+      country_code: 'sa',
+    },
+    {
+      code: 'hr',
+      name: 'עִברִית',
+      dir: 'rtl',
+      country_code: 'il',
+    },
+  ]
+  
 
 const HeroSlide = () => {
 
@@ -53,14 +76,21 @@ const HeroSlide = () => {
                   ))
               }
           </Swiper>
-          {
-              movieItems.map((item, i) => <TrailerModal key={i} item={item}/>)
-          }
       </div>
   );
 }
 
 const HeroSlideItem = props => {
+
+  const currentLanguageCode = cookies.get('i18next') || 'en'
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    console.log('Setting page stuff')
+    document.body.dir = currentLanguage.dir || 'ltr'
+    document.title = t('app_title')
+  }, [currentLanguage, t])
 
   let hisrory = useHistory();
 
@@ -84,7 +114,7 @@ const HeroSlideItem = props => {
                       <p>{item.overview}</p>
                       <p className='PDetails'>
                         <span className='SomeDetails voteAvg'>
-                            <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--star-inline" id="iconContext-star-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path></svg>
+                            <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg" className="ipc-icon ipc-icon--star-inline" id="iconContext-star-inline" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path></svg>
                             {item.vote_average}
                         </span>
                         <span className='SomeDetails'>{TakeYear(item.release_date)}</span>
@@ -92,7 +122,7 @@ const HeroSlideItem = props => {
                   </div>
                   <div className="btns">
                       <Button onClick={() => hisrory.push('/movie/' + item.id)}>
-                          Details
+                        {t('details')}
                       </Button>
                   </div>
               </div>
@@ -101,22 +131,6 @@ const HeroSlideItem = props => {
               </div>
           </div>
       </div>
-  )
-}
-
-const TrailerModal = props => {
-  const item = props.item;
-
-  const iframeRef = useRef(null);
-
-  const onClose = () => iframeRef.current.setAttribute('src', '');
-
-  return (
-      <Modal active={false} id={`modal_${item.id}`}>
-          <ModalContent onClose={onClose}>
-              <iframe ref={iframeRef} width="100%" height="500px" title="trailer"></iframe>
-          </ModalContent>
-      </Modal>
   )
 }
 
