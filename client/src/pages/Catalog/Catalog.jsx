5fe2ca467831351717dc17eacf168029fc369a0b
HopeMashal <mashal.hope@gmail.com>
2022-04-16 17:40:56 +0300

    edit catalog file

--- client/src/pages/Catalog/Catalog.jsx
+++ client/src/pages/Catalog/Catalog.jsx
@@ -7,14 +7,18 @@ import PageHeader from '../../components/PageHeader/PageHeader';
 import { category as cate } from '../../apis/apiTMDB';
 import MovieGrid from '../../components/MovieGrid/MovieGrid';
 
+import { useTranslation } from 'react-i18next'
+
 const Catalog = () => {
 
     const { category } = useParams();
 
+    const { t } = useTranslation()
+
     return (
         <>
             <PageHeader>
-                {category === cate.movie ? 'Movies' : 'TV Series'}
+                {category === cate.movie ? `${t('movie-text')}` : `${t('tv-series-text')}`}
             </PageHeader>
             <div className="container">
                 <div className="section mb-3">
